ab8deb3bee13ad8a021ef7eeef6af9aa7e16b6e9
Thuan Nguyen <235thuan@gmail.com>
2019-05-03 08:52:13 -0700

    day lan 1

---
+++ Luyện tập với cấu trúc switch-case/switchcase3.js
@@ -0,0 +1,11 @@
+let giatri = prompt('Type a number', 0);
+if (giatri > 0) {
+    alert(1);
+}
+    else if (giatri < 0) {
+        alert(-1);
+    }
+    else {
+        alert(0);
+    }
+
---
+++ Luyện tập với cấu trúc switch-case/switchcase3b.js
@@ -0,0 +1,11 @@
+let giatri = prompt('Type a number', 0);
+switch (giatri){
+    case (giatri>0):
+        alert(1);
+        break;
+    case (giatri<0):
+        alert(-1);
+        break;
+    default:
+        alert(0);
+}
